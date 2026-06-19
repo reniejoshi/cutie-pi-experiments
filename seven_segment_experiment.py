b9470089451ff249f399a46a3640db99dd8cb516
reniejoshi <renie_joshi@yahoo.com>
2026-06-18 21:50:41 -0700

    Fix pin out

--- seven_segment_experiment.py
+++ seven_segment_experiment.py
@@ -1,7 +1,7 @@
 from gpiozero import LEDCharDisplay
 from time import sleep
 
-display = LEDCharDisplay(3, 5, 7, 8, 10, 11, 12, 16)
+display = LEDCharDisplay(2, 3, 4, 17, 14, 15, 16, 18, 27, dp=11)
 
 for char in '0123456789':
     display.value = char
