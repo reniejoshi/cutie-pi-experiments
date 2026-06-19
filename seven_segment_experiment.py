764ad2b3244ffecb8358c97a6ce3e30885c8c0b6
reniejoshi <renie_joshi@yahoo.com>
2026-06-18 21:51:59 -0700

    Fix dp pin

--- seven_segment_experiment.py
+++ seven_segment_experiment.py
@@ -1,7 +1,7 @@
 from gpiozero import LEDCharDisplay
 from time import sleep
 
-display = LEDCharDisplay(2, 3, 4, 17, 14, 15, 16, 18, 27, dp=11)
+display = LEDCharDisplay(2, 3, 4, 14, 15, 16, 18, 27, dp=17)
 
 for char in '0123456789':
     display.value = char
