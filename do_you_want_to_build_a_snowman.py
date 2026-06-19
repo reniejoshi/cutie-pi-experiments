f953cc8dfd90be252d978b08d11ed3ecd499cd39
reniejoshi <renie_joshi@yahoo.com>
2026-06-19 14:37:06 -0700

    Fix do you want to build a snowman

--- do_you_want_to_build_a_snowman.py
+++ do_you_want_to_build_a_snowman.py
@@ -3,39 +3,35 @@ from time import sleep, time
 
 NOTE_G4  = 392
 NOTE_A4  = 440
+NOTE_B4  = 494
 NOTE_C5  = 523
 NOTE_D5  = 587
-NOTE_Eb5 = 622
+NOTE_E5  = 659
 NOTE_F5  = 698
 NOTE_G5  = 784
 
 melody = [
     NOTE_G4, NOTE_G4, NOTE_G4, 0,
-    
-    NOTE_C5, NOTE_D5, NOTE_Eb5, NOTE_F5, NOTE_G5, NOTE_F5,
-    
-    NOTE_Eb5, NOTE_D5, NOTE_C5, NOTE_D5, NOTE_Eb5, NOTE_C5, 0,
-    
-    NOTE_C5, NOTE_D5, NOTE_Eb5, NOTE_F5, NOTE_G5, NOTE_Eb5,
-    
-    NOTE_F5, NOTE_G5, NOTE_F5, 0,
-    
-    NOTE_D5, NOTE_Eb5, NOTE_F5, NOTE_D5, NOTE_C5
+    NOTE_G4, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_D5,
+    NOTE_C5, NOTE_D5, NOTE_E5, NOTE_C5, NOTE_D5, NOTE_G4, 0,
+    NOTE_G4, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_D5,
+    NOTE_E5, NOTE_C5, NOTE_D5, 0,    
+    NOTE_B4, NOTE_C5, NOTE_D5, NOTE_B4, NOTE_C5
 ]
 
 note_durations = [
     16, 16, 16, 4,
-    4, 8, 8, 8, 4, 2,
-    4, 8, 8, 8, 4, 4, 4,
-    4, 8, 8, 8, 4, 4,
-    4, 4, 2, 4,
-    4, 8, 8, 4, 1
+    4, 4, 4, 4, 2, 2,
+    4, 4, 4, 4, 2, 2, 4,
+    4, 4, 4, 4, 2, 2,
+    2, 2, 2, 4,
+    4, 4, 4, 4, 1
 ]
 
-buzzer = PWMOutputDevice(2)
+buzzer = PWMOutputDevice(18)
 
 def play_snowman():
-    tempo = 1.6
+    tempo = 1.4
     
     for note, duration in zip(melody, note_durations):
         note_duration = tempo / duration
