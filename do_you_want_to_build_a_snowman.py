16047140703c3cae78a3e5ac8a3d86eee4c8c04d
reniejoshi <renie_joshi@yahoo.com>
2026-06-19 14:34:20 -0700

    Implement do you want to build a snowman

---
+++ do_you_want_to_build_a_snowman.py
@@ -0,0 +1,58 @@
+from gpiozero import PWMOutputDevice
+from time import sleep, time
+
+NOTE_G4  = 392
+NOTE_A4  = 440
+NOTE_C5  = 523
+NOTE_D5  = 587
+NOTE_Eb5 = 622
+NOTE_F5  = 698
+NOTE_G5  = 784
+
+melody = [
+    NOTE_G4, NOTE_G4, NOTE_G4, 0,
+    
+    NOTE_C5, NOTE_D5, NOTE_Eb5, NOTE_F5, NOTE_G5, NOTE_F5,
+    
+    NOTE_Eb5, NOTE_D5, NOTE_C5, NOTE_D5, NOTE_Eb5, NOTE_C5, 0,
+    
+    NOTE_C5, NOTE_D5, NOTE_Eb5, NOTE_F5, NOTE_G5, NOTE_Eb5,
+    
+    NOTE_F5, NOTE_G5, NOTE_F5, 0,
+    
+    NOTE_D5, NOTE_Eb5, NOTE_F5, NOTE_D5, NOTE_C5
+]
+
+note_durations = [
+    16, 16, 16, 4,
+    4, 8, 8, 8, 4, 2,
+    4, 8, 8, 8, 4, 4, 4,
+    4, 8, 8, 8, 4, 4,
+    4, 4, 2, 4,
+    4, 8, 8, 4, 1
+]
+
+buzzer = PWMOutputDevice(2)
+
+def play_snowman():
+    tempo = 1.6
+    
+    for note, duration in zip(melody, note_durations):
+        note_duration = tempo / duration
+        
+        if note == 0:
+            sleep(note_duration)
+        else:
+            buzzer.frequency = note
+            buzzer.value = 0.5
+            
+            sleep(note_duration * 0.85)
+            buzzer.value = 0
+            sleep(note_duration * 0.15)
+
+try:
+    play_snowman()
+except KeyboardInterrupt:
+    buzzer.off()
+finally:
+    buzzer.close()
